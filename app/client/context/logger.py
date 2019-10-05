f3a0a5638a44cbfae837443444211d833e711cc2
nlivni <livni55@gmail.com>
2019-10-06 02:49:34 +0300

    remove logs to file in client

--- app/client/context/logger.py
+++ app/client/context/logger.py
@@ -2,11 +2,6 @@ import logging
 from app.client.context import config
 
 format = '%(asctime)s %(name)s %(levelname)s %(message)s'
-fmt = logging.Formatter(format)
 level = config['logger']['level']
-logging.basicConfig(format=format, level=level, filename=config['logger']['path'])
+logging.basicConfig(format=format, level=level)
 log = logging.getLogger('clinet')
-console_handler = logging.StreamHandler()
-console_handler.setLevel(level)
-console_handler.setFormatter(fmt)
-log.addHandler(console_handler)
